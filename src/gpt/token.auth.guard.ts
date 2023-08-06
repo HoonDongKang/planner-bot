80d6c626ac5f8d500dbc1c7ee4427d35400d894e
HoonDongKang <d159123@naver.com>
2023-08-06 23:28:59 +0900

    .

--- src/gpt/token.auth.guard.ts
+++ src/gpt/token.auth.guard.ts
@@ -14,14 +14,13 @@ export class TokenGuard implements CanActivate {
   ): boolean | Promise<boolean> | Observable<boolean> {
     const request = context.switchToHttp().getRequest();
     const { gpt_token } = request.cookies;
-    console.log(`gpt_token : ${gpt_token}`);
-    return true;
-    // if (!gpt_token) return true;
-    // else {
-    //   throw new HttpException(
-    //     '5회 무료 이용이 끝났습니다.',
-    //     HttpStatus.FORBIDDEN,
-    //   );
-    // }
+
+    if (!gpt_token) return true;
+    else {
+      throw new HttpException(
+        '5회 무료 이용이 끝났습니다.',
+        HttpStatus.FORBIDDEN,
+      );
+    }
   }
 }
